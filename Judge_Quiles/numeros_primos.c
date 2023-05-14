f4be8a40b3b4e3c18d506e97cba04eabeb17b65c
joao-zip <oliveira.joao@unifesp.br>
2023-05-14 16:37:50 -0300

    tarefa 02 - numeros primos

---
+++ Judge_Quiles/numeros_primos.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+
+int main () {
+   
+    int ini_intervalo, fim_intervalo, eh_primo;
+
+    scanf("%d %d", &ini_intervalo, &fim_intervalo);
+
+    for(int i = ini_intervalo; i <= fim_intervalo; i++) {
+
+        eh_primo = TRUE; 
+        for(int j = 2; j < i; j++) {
+            if(i % j == 0){
+                eh_primo = FALSE;
+            }
+        }
+
+        if(eh_primo == TRUE) {
+            printf("%d ", i);
+        }
+        
+    }
+
+    return 0;
+}
